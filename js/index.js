737d69a3caa2266119d34fd1da7cf5653076b50f
KauanMO <35741475+melissa-cavalcante@users.noreply.github.com>
2024-07-28 01:08:10 -0300

    lista de tarefas com a lógica completa

--- js/index.js
+++ js/index.js
@@ -4,6 +4,81 @@ const todoListUl = document.querySelector("#todo-list")
 
 let task = [];
 
+function renderTaskOnHTML(titleTask, done = false){
+    const li = document.createElement("li");
+
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute('type', 'checkbox');
+
+    checkbox.addEventListener('change', (event) => {
+        const liToToggle = event.target.parentElement;
+
+        const spanToToggle = liToToggle.querySelector('span');
+
+        const done = event.target.checked;
+
+        if(done){
+            spanToToggle.style.textDecoration = "line-through";
+        } else {
+            spanToToggle.style.textDecoration = "none";
+        }
+
+        task = task.map(t => {
+            if (t.title === spanToToggle.textContent) {
+                return {
+                    title: t.title,
+                    done: !t.done,
+                }
+            }
+
+            return t;
+        })
+
+        localStorage.setItem("task", JSON.stringify(task))
+
+    });
+
+    checkbox.checked = done;
+
+    const span = document.createElement("span");
+    span.textContent = titleTask;
+
+    if(done){
+        span.style.textDecoration = "line-through"
+    }
+
+    const button = document.createElement("button");
+    button.textContent = "Remover";
+    button.addEventListener("click", (event) => {
+        const liToRemove = event.target.parentElement; 
+
+        const titleToRemove = liToRemove.querySelector("span").textContent;
+
+        task = task.filter(t => t.title !== titleToRemove);
+
+        todoListUl.removeChild(liToRemove); 
+
+    })
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(button);
+
+    todoListUl.appendChild(li)
+}
+
+window.onload = () => {
+    const taskOnLocalStorage = localStorage.getItem("task")
+
+        if(!taskOnLocalStorage) return
+
+        task = JSON.parse(taskOnLocalStorage)
+
+        task.forEach(t => {
+            renderTaskOnHTML(t.title, t.done)
+        })
+}
+
 form.addEventListener("submit", (event) => {
     event.preventDefault() // evita o comportamento padrão de recarregar a página ao submeter o fomulário.
 
@@ -14,18 +89,16 @@ form.addEventListener("submit", (event) => {
         return; // o return serve para sair da função, ou seja, se o titleTask for menor doq tres caracteres, vai mostrar o alerta, e assim que mostrar o alerta ele nao vai executar mais nada, pq o return vai sair da função, encerra toda a função
     }
 
-    task.push(titleTask);
+    task.push({
+        title: titleTask,
+        done: false,
+    });
 
-    const li = document.createElement("li")
-    const span = document.createElement("span");
-    const checkbox = document.createElement("input");
-    const button = document.createElement("button");
-    
-    checkbox.setAttribute('type', 'checkbox')
+    localStorage.setItem("task", JSON.stringify(task))
 
-    checkbox 
-    span.textContent = titleTask;
-    todoListUl.appendChild(span)
+    renderTaskOnHTML(titleTask)
+
 
     input_todo.value = "";
+
 });
